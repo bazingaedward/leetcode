cd9da45c820631156444d506d7bec0f666a623d5
bazingaedward <bazingaedward@2893381.no-reply.drupal.org>
2021-05-05 16:43:22 +0800

    update: 15

---
+++ questions/15-threeSum/index.js
@@ -0,0 +1,27 @@
+import { quickSort } from '../../utils/array'
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ export default threeSum = function(nums) {
+    var arr = quickSort(nums)
+    var res = []
+    for(i=0;i<arr.length - 2;i++){
+        var j=i+1,k=arr.length-1
+        while(j<k){
+            var s = arr[i]+arr[j]+arr[k]
+            if(s<0){
+                while(j<k && arr[j]==arr[j+1])j++
+            }else if(s>0){
+                while(j<k && arr[k-1]==arr[k])k--
+            }else{
+                res.push([
+                    arr[i],
+                    arr[j],
+                    arr[k]
+                ])
+            }
+        }
+    }
+    return res
+}
