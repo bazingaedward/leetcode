ea3d778712bc6203d7c7b520df160c498ecdce69
bazingaedward <bazingaedward@2893381.no-reply.drupal.org>
2021-05-05 17:48:34 +0800

    update: 15

--- questions/15-threeSum/index.js
+++ questions/15-threeSum/index.js
@@ -4,22 +4,29 @@ import { quickSort } from '../../utils/array'
  * @return {number[][]}
  */
  export const threeSum = function(nums) {
+    // 边界case
+    if(nums.length < 3) return []
+
     var arr = quickSort(nums)
     var res = []
     for(var i=0;i<arr.length - 2;i++){
+        if(arr[i]>0)break
+        if(i>0 && arr[i] == arr[i-1])continue
         var j=i+1,k=arr.length-1
         while(j<k){
             var s = arr[i]+arr[j]+arr[k]
             if(s<0){
-                while(j<k && arr[j]==arr[j+1])j++
+                j++
+                while(j<k && arr[j]==arr[j-1])j++
             }else if(s>0){
-                while(j<k && arr[k-1]==arr[k])k--
+                k--
+                while(j<k && arr[k+1]==arr[k])k--
             }else{
-                res.push([
-                    arr[i],
-                    arr[j],
-                    arr[k]
-                ])
+                res.push([arr[i],arr[j],arr[k]])
+                j++
+                k--
+                while(j<k && arr[j]==arr[j-1])j++
+                while(j<k && arr[k]==arr[k+1])k--
             }
         }
     }
--- questions/15-threeSum/index.test.js
+++ questions/15-threeSum/index.test.js
@@ -7,6 +7,9 @@ import {threeSum} from './index'
 describe('three sum', ()=>{
     it("[-1,0,1,2,-1,-4]", ()=>{
         const res = threeSum([-1,0,1,2,-1,-4])
-        expect(res).toEqual([])
+        expect(res).toEqual([
+            [-1,-1,2],
+            [-1,0,1]
+        ])
     })
 })
